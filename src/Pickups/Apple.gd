499c84e79984bc33e2ecdce681a00d59c67929c4
h3nse <henrikf.bj@gmail.com>
2023-02-23 09:25:05 +0100

    Add different healthpack types

--- src/Pickups/Apple.gd
+++ src/Pickups/Apple.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if PlayerStats.health != PlayerStats.max_health:
-		PlayerStats.health = PlayerStats.max_health
+		PlayerStats.health += 1
 		queue_free()
