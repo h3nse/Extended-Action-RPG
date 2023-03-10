f946ec6fc9a16e89647a194fe0d10cfcab452da3
Noah <noah.linde@outlook.com>
2023-03-10 11:28:35 +0100

    Add whip up'n'down anim, squash apple bat bug

--- src/Pickups/Apple.gd
+++ src/Pickups/Apple.gd
@@ -2,6 +2,6 @@ extends Node2D
 export var health_increase = 1
 
 func _on_Area2D_body_entered(body):
-	if PlayerStats.health != PlayerStats.max_health:
+	if !PlayerStats.is_max_health():
 		PlayerStats.health += health_increase
 		queue_free()
