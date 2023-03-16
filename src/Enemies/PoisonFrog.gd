073cffc48317b6f013ca3827bc5a93a8c057882b
h3nse <henrikf.bj@gmail.com>
2023-03-16 19:37:57 +0100

    Add 1. stage movement for poison frog

--- src/Enemies/PoisonFrog.gd
+++ src/Enemies/PoisonFrog.gd
@@ -1,15 +1,32 @@
 extends KinematicBody2D
 
+#export variables
+export var jumpLength = 1000
+
+#Variables
+onready var MoveTimer = $MoveTimer
+
+var velocity = Vector2.ZERO
+
 #States
 enum {
 	WANDER,
 	CHASE
 }
 var state = WANDER
-#Damage and hurt
+
 func _process(delta):
 	match state:
 		WANDER:
-			pass
+			_on_MoveTimer_timeout()
 		CHASE:
 			pass
+
+func _on_MoveTimer_timeout():
+	velocity = get_random_direction() * jumpLength
+	velocity = move_and_slide(velocity)
+	MoveTimer.start(rand_range(2,10))
+
+func get_random_direction():
+	var direction = Vector2(rand_range(-100,100), rand_range(-100,100))
+	return direction.normalized()
