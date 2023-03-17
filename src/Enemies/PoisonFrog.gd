a5f1180277d6f68d8c737e6c878a679bb09a4cfd
h3nse <henrikf.bj@gmail.com>
2023-03-17 09:20:11 +0100

    Update poisonfrog movement

--- src/Enemies/PoisonFrog.gd
+++ src/Enemies/PoisonFrog.gd
@@ -1,31 +1,45 @@
 extends KinematicBody2D
 
 #export variables
-export var jumpLength = 1000
+export var maxJumpLength = 5
+export var jumpInterval = 10
+export var friction = 10
+export var moveSpeed = 10
 
 #Variables
-onready var MoveTimer = $MoveTimer
+onready var moveTimer = $MoveTimer
+onready var jumpTimer = $JumpTimer
 
 var velocity = Vector2.ZERO
 
 #States
 enum {
-	WANDER,
+	IDLE,
+	JUMP,
 	CHASE
 }
-var state = WANDER
+var state = IDLE
+
+func _ready():
+	moveTimer.wait_time = rand_range(2, 10)
 
 func _process(delta):
 	match state:
-		WANDER:
-			pass
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		JUMP:
+			velocity = move_and_slide(velocity)
 		CHASE:
 			pass
 
 func _on_MoveTimer_timeout():
-	velocity = get_random_direction() * jumpLength
-	velocity = move_and_slide(velocity)
-	MoveTimer.start(rand_range(2,10))
+	jumpTimer.start(rand_range(1, maxJumpLength))
+	velocity = get_random_direction() * moveSpeed
+	state = JUMP
+
+func _on_JumpTimer_timeout():
+	moveTimer.start(rand_range(1, jumpInterval))
+	state = IDLE
 
 func get_random_direction():
 	var direction = Vector2(rand_range(-100,100), rand_range(-100,100))
