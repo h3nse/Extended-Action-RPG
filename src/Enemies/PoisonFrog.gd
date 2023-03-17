34c72b96671120f0b6988d62b49e9896f11ceeee
h3nse <henrikf.bj@gmail.com>
2023-03-17 14:04:36 +0100

    Update frog movement

--- src/Enemies/PoisonFrog.gd
+++ src/Enemies/PoisonFrog.gd
@@ -11,6 +11,8 @@ onready var moveTimer = $MoveTimer
 onready var jumpTimer = $JumpTimer
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 var velocity = Vector2.ZERO
 
@@ -23,20 +25,24 @@ enum {
 var state = IDLE
 
 func _ready():
+	randomize()
 	moveTimer.wait_time = rand_range(2.0, jumpInterval)
 
 func _process(delta):
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+			animationTree.set("parameters/Idle/blend_position", velocity)
+			animationState.travel("Idle")
 		JUMP:
 			velocity = move_and_slide(velocity)
+			animationTree.set("parameters/Jump/blend_position", velocity)
+			animationState.travel("Jump")
 		CHASE:
 			pass
-	sprite.flip_h = velocity.x < 0
 
 func _on_MoveTimer_timeout():
-	jumpTimer.start(rand_range(1, maxJumpLength))
+	jumpTimer.start(rand_range(0.25, maxJumpLength))
 	velocity = get_random_direction() * moveSpeed
 	state = JUMP
 
